e2a7523d3ca11cd3e2e9438071a62286d7ee0155
ZagloSs <carlosdiezmendez@gmail.com>
2023-11-06 12:27:33 +0100

    Login Hecho

--- app/src/main/java/com/example/giwi/Login.java
+++ app/src/main/java/com/example/giwi/Login.java
@@ -2,7 +2,9 @@ package com.example.giwi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -12,33 +14,65 @@ import android.widget.Toast;
 import com.example.giwi.Database.DatabaseAux;
 
 public class Login extends AppCompatActivity {
+    private DatabaseAux aux;
+    private SQLiteDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         Toast.makeText(this, "holaa", Toast.LENGTH_SHORT).show();
+        aux = new DatabaseAux(Login.this);
+        db = aux.getWritableDatabase();
+
     }
     public void changeToSignin(View view){
         Intent nIntent = new Intent(Login.this, SignIn.class);
         startActivity(nIntent);
     }
+    @SuppressLint("Range")
     public void logIn(View view){
-        TextView emailTextView = findViewById(R.id.emailSingin);
-        TextView passTextView = findViewById(R.id.passSingin);
+
+        TextView emailTextView = findViewById(R.id.emailLogin);
+        TextView passTextView = findViewById(R.id.passLogin);
 
         String emailString = emailTextView.getText().toString();
         String passString = passTextView.getText().toString();
 
 
-        DatabaseAux aux = new DatabaseAux(Login.this);
-        SQLiteDatabase db = aux.getWritableDatabase();
 
             if(db != null && !emailString.isEmpty() && !passString.isEmpty()) {
-                Intent nIntent = new Intent(Login.this, Profile.class);
-                startActivity(nIntent);
+                String pasarName="loading";
+                String pasarEmail="loading";
+                String compPass="";
+
+                    Cursor c = db.rawQuery("SELECT name, password FROM users WHERE email ='" +emailString+ "' AND password ='" +passString+ "'", null);
+                    if(c.moveToFirst()){
+                        pasarName = c.getString(c.getColumnIndex("name"));
+                        compPass = c.getString(c.getColumnIndex("password"));
+                    }
+                    pasarEmail = emailString;
+
+                   if(compPass.equals(passString)){
+                       Intent nIntent = new Intent(Login.this, Profile.class);
+                       nIntent.putExtra("name", pasarName);
+                       nIntent.putExtra("email", pasarEmail);
+                       startActivity(nIntent);
+                   }else{
+                       Toast.makeText(this, "Intentelo de nuevo", Toast.LENGTH_SHORT).show();
+
+                   }
+
+                }
+
             }
+
         }
-}
+
+
+
+
+
 
 
--- app/src/main/java/com/example/giwi/Profile.java
+++ app/src/main/java/com/example/giwi/Profile.java
@@ -2,13 +2,43 @@ package com.example.giwi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
+
+    private TextView tvName, tvEmail;
+    private String name, email;
+
+    private FirebaseAuth authProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        tvName = findViewById(R.id.nameView);
+        tvEmail = findViewById(R.id.emailView);
+
+       Intent intent = getIntent();
+
+       String nameR = intent.getStringExtra("name");
+       String emailR = intent.getStringExtra("email");
+
+        tvName.setText(nameR);
+        tvEmail.setText(emailR);
+
+
+
+    }
+
+    public void changeToLogin(View view){
+        Intent nIntent = new Intent(Profile.this, Login.class);
+        startActivity(nIntent);
+        finish();
     }
 }
