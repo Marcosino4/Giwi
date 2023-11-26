2f527fb1c58763cc7d6b9f097653c382f636f411
SaqFernando <jean_cangrinaje@hotmail.com>
2023-11-26 14:48:11 +0100

    Agregado las demas ubicaciones

---
+++ app/src/main/java/com/example/giwi/Praga_View.java
@@ -0,0 +1,14 @@
+package com.example.giwi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Praga_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_praga_view);
+    }
+}
---
+++ app/src/main/java/com/example/giwi/Viena_View.java
@@ -0,0 +1,14 @@
+package com.example.giwi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Viena_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viena_view);
+    }
+}
