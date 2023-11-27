cf2b208e17d9d276e0479101ae931e03f9fb7d8a
ZagloSs <carlosdiezmendez@gmail.com>
2023-11-27 14:21:43 +0100

    Registro añadido (falta arreglar el manifest)
    sd

--- app/src/main/java/com/example/giwi/SignIn.java
+++ app/src/main/java/com/example/giwi/SignIn.java
@@ -12,10 +12,15 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import com.example.giwi.Database.DatabaseAux;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -34,57 +39,47 @@ public class SignIn extends AppCompatActivity {
         startActivity(nIntent);
     }
 
-    public void insertValues(View v){
+    public void signup(){
         TextView nameTextView = findViewById(R.id.nameSingin);
         TextView lastNameTextView = findViewById(R.id.lastNameSingin);
         TextView emailTextView = findViewById(R.id.emailSingin);
         TextView passTextView = findViewById(R.id.passSingin);
 
+        //Pasar lo que hay en el textview a string
         String nameString = nameTextView.getText().toString();
         String lastNameString = lastNameTextView.getText().toString();
         String emailString = emailTextView.getText().toString();
         String passString = passTextView.getText().toString();
 
+        if(!nameString.isEmpty() && !lastNameString.isEmpty() && !emailString.isEmpty() && !passString.isEmpty()){
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.createUserWithEmailAndPassword(emailString, passString).addOnCompleteListener(SignIn.this,
+                    new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                //Toast para indicar que el usuario ha sido registrado
+                                Toast.makeText(SignIn.this, "Usuario Registrado", Toast.LENGTH_LONG).show();
+                                FirebaseUser user = auth.getCurrentUser();
 
-        DatabaseAux aux = new DatabaseAux(SignIn.this);
-        SQLiteDatabase db = aux.getWritableDatabase();
+                                //Mandar correo de verificacion
+                                user.sendEmailVerification();
 
-        if(db != null && !nameString.isEmpty() && !lastNameString.isEmpty() && !emailString.isEmpty() && !passString.isEmpty()) {
-            ContentValues values = new ContentValues();
+                                //Una vez registrado el usuario, accede a la pantalla de home
+                                Intent intent =  new Intent(SignIn.this, Home.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                                        Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
 
-            values.put("name", nameString);
-            values.put("lastName", lastNameString);
-            values.put("email", emailString);
-            values.put("password", passString);
+                                finish();
 
-
-            long res = db.insert( "users", null, values);
-
-            if (res >= 0){
-                Toast.makeText(this, "Insertado correctamente", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(SignIn.this, Login.class));
-            }else{
-                Toast.makeText(this, "Fallo al insertar", Toast.LENGTH_LONG).show();
-            }
-            db.close();
+                            }else{
+                                Toast.makeText(SignIn.this, "Error: Usuario no registrado", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }else{
+            Toast.makeText(SignIn.this, "Porfavor, rellene los campos", Toast.LENGTH_LONG).show();
         }
-        FirebaseFirestore firestoreDb = FirebaseFirestore.getInstance();
-        Map<String, Object> users = new HashMap<>();
-        users.put("name", nameString);
-        users.put("lastName", lastNameString);
-        users.put("email", emailString);
-        users.put("password", passString);
-
-        firestoreDb.collection("Giwi").document(emailString).set(users).addOnSuccessListener(new OnSuccessListener<Void>(){
-            @Override
-            public void onSuccess(Void unused) {
-                Log.d("DEBUG", "TODO OK");
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("ERROR", e.getMessage());
-            }
-        });
     }
 }
